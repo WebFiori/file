56daab720fae8141f80dcc9a308930469e4248ac
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-07-12 00:26:33 +0300

    Update UploaderTest.php

--- tests/webfiori/framework/test/UploaderTest.php
+++ tests/webfiori/framework/test/UploaderTest.php
@@ -95,7 +95,7 @@ class UploaderTest extends TestCase {
         
         $this->assertEquals(0,$file->getUploadError());
         $this->assertEquals("{\"id\":-1,\"mime\":\"text\/plain\",\"name\":\"testUpload.txt\""
-                . ",\"directory\":\"".Json::escapeJSONSpecialChars(ROOT_DIR."tests\\webfiori\\framework\\test")."\",\"sizeInBytes\":0,"
+                . ",\"directory\":\"".Json::escapeJSONSpecialChars(ROOT_DIR."tests\webfiori\framework\test")."\",\"sizeInBytes\":0,"
                 . "\"sizeInKBytes\":0,\"sizeInMBytes\":0,\"uploaded\":false,\"isReplace\":false,\"uploadError\":0}", $file.'');
     }
 }
