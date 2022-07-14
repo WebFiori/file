b66a66eeb6363aabbcf2b04bd8c71bab654ff0c6
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-07-15 02:24:54 +0300

    Create upload-file.php

---
+++ example/upload-file.php
@@ -0,0 +1,25 @@
+<?php
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+require_once '../vendor/autoload.php';
+
+
+use webfiori\file\Uploader;
+use webfiori\file\UploadFile;
+
+$u = new Uploader(__DIR__.DIRECTORY_SEPARATOR.'uploads');
+$u->addExts([
+    'txt', 'doc', 'docx', 'png', 'jpg'
+]);
+$files = $u->uploadAsFileObj();
+$file = $files[0];
+if ($file instanceof UploadFile) {
+    if (!$file->isUploaded()) {
+        http_response_code(404);
+        echo 'File not uploaded due to error code: '.$file->getUploadError();
+    } else {
+        echo 'Successfully uploaded.';
+    }
+}
